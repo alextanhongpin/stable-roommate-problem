162f87e8a591f1661f4e855809ec2f0ce7d59ec1
Alex Tan Hong Pin <alextan220990@gmail.com>
2020-12-05 16:59:52 +0800

    chore: cleanup solution

--- stable-roommate-problem.v2.js
+++ stable-roommate-problem.v2.js
@@ -1,15 +1,16 @@
-// From wiki.
-const preferences = {
-  1: [3, 4, 2, 6, 5],
-  2: [6, 5, 4, 1, 3],
-  3: [2, 4, 5, 1, 6],
-  4: [5, 2, 3, 6, 1],
-  5: [3, 1, 2, 4, 6],
-  6: [5, 1, 3, 4, 2]
+// From wikipedia.
+const preferences1 = {
+  "1": ["3", "4", "2", "6", "5"],
+  "2": ["6", "5", "4", "1", "3"],
+  "3": ["2", "4", "5", "1", "6"],
+  "4": ["5", "2", "3", "6", "1"],
+  "5": ["3", "1", "2", "4", "6"],
+  "6": ["5", "1", "3", "4", "2"]
 }
 
+
 // From youtube. Search for Irving's algorithm.
-const preferences1 = {
+const preferences2 = {
   a: 'b d f c e'.split(' '),
   b: 'd e f a c'.split(' '),
   c: 'd e f a b'.split(' '),
@@ -19,7 +20,7 @@ const preferences1 = {
 }
 
 // http://www.dcs.gla.ac.uk/~pat/jchoco/roommates/papers/Comp_sdarticle.pdf
-const preferences2 = {
+const preferences3 = {
   1: [2, 5, 4, 6, 7, 8, 3],
   2: [3, 6, 1, 7, 8, 5, 4],
   3: [4, 7, 2, 8, 5, 6, 1],
@@ -30,7 +31,8 @@ const preferences2 = {
   8: [5, 4, 7, 1, 2, 3, 6]
 }
 
-const preferences3 = {
+
+const preferences4 = {
   1: [28, 4, 11, 5, 30, 7, 12, 2, 3, 9, 10, 15, 20, 21, 24, 25, 23, 13, 19, 6, 8, 14, 16, 17, 18, 22, 26, 27, 29],
   2: [22, 24, 14, 27, 10, 8, 26, 29, 6, 16, 17, 12, 21, 5, 13, 18, 19, 23, 15, 1, 4, 9, 11, 25, 30, 3, 7, 20, 28],
   3: [22, 18, 26, 6, 16, 17, 23, 29, 24, 5, 8, 13, 14, 19, 27, 2, 15, 20, 28, 1, 4, 7, 9, 10, 11, 12, 21, 25, 30],
@@ -63,171 +65,130 @@ const preferences3 = {
   30: [23, 5, 4, 11, 28, 13, 19, 6, 8, 14, 16, 17, 18, 22, 26, 27, 29, 7, 12, 1, 2, 3, 9, 10, 15, 20, 21, 24, 25]
 }
 
-function defined(o) {
-  return o !== null && o !== undefined
+function stringify(o) {
+  const r = {}
+  for (let k in o) {
+    r[k] = o[k].map(i => i.toString())
+  }
+  return r
+}
+
+function clone(o) {
+  const c = {}
+  for (let key in o) {
+    c[key] = [...o[key]]
+  }
+  return c
 }
 
 function stableRoommateProblem(preferences) {
-  validate(preferences)
+  const people = Object.keys(preferences)
   const accepted = {}
-  const preferenceKeys = Object.keys(preferences)
-  // Stage 1: Generate preference list.
-  while (preferenceKeys.length) {
-    const p = preferenceKeys.shift()
-    const q = preferences[p]?. [0]
-    const o = accepted[q]
-    if (o) {
-      // Check which one q preferred.
-      const l = preferences[q].indexOf(o)
-      const r = preferences[q].indexOf(p)
-      const i = l < r ? r : l
-      const x = preferences[q].slice(i)
-      for (let i of x) {
-        preferences[i] = preferences[i].filter((it) => it != q)
+  // Phase 1.
+  while (people.length) {
+    const currProposer = people.shift()
+    const proposed = preferences[currProposer][0]
+    const prevProposer = accepted[proposed]
+
+    if (prevProposer) {
+      const prevRank = preferences[proposed].indexOf(prevProposer)
+      const currRank = preferences[proposed].indexOf(currProposer)
+
+      // Lower the index, the higher the preference.
+      const idx = prevRank < currRank ? currRank : prevRank
+      const rejects = prevRank < currRank ? currProposer : prevProposer
+      const accepts = prevRank < currRank ? prevProposer : currProposer
+      people.unshift(rejects)
+      preferences[rejects].shift()
+      const rejected = preferences[proposed].slice(idx)
+      for (const r of rejected) {
+        preferences[r] = preferences[r].filter(person => person !== proposed)
       }
-      preferences[q] = preferences[q].slice(0, i)
-      preferenceKeys.unshift(l < r ? p : o) // Re-propose for rejected.
-      accepted[q] = l < r ? o : p
+      preferences[proposed] = preferences[proposed].slice(0, idx)
+      accepted[proposed] = accepts
     } else {
-      accepted[q] = p
+      accepted[proposed] = currProposer
     }
   }
 
-  // Stage 2: Reject all after the proposed.
-  for (let key in preferences) {
-    const prefs = preferences[key]
-    const idx = prefs.indexOf(accepted[key])
-    if (idx > -1) {
-      const lessPreferred = prefs.slice(idx + 1, prefs.length)
-      for (let i of lessPreferred) {
-        preferences[i] = preferences[i].filter(it => it !== key)
-      }
-      preferences[key] = prefs.slice(0, idx + 1)
+  // Phase 2: Reject those lower than the preferences
+  for (let proposer in preferences) {
+    const idx = preferences[proposer].indexOf(accepted[proposer])
+    if (idx === -1) continue
+    const kept = preferences[proposer].slice(0, idx + 1)
+    const reject = preferences[proposer].slice(idx + 1)
+    for (const rejected of reject) {
+      preferences[rejected] = preferences[rejected].filter(person => person !== proposer)
     }
+    preferences[proposer] = kept
   }
 
-  if (Object.values(preferences).some(item => item.length === 0)) {
-    throw new Error("no stable matching, some results are empty")
-  }
-
-  while (Object.values(preferences).some(item => item.length > 1)) {
-    const idx = Object.values(preferences).findIndex(item => item.length > 1)
-    const rotation = getRotation(preferences, Object.keys(preferences)[idx]).reverse()
-    if (rotation.length % 2 !== 0) throw new Error('no stable rotation')
-
-    const rotated = []
-    while (rotation.length) {
-      const pair = rotation.splice(0, 2)
-      rotated.push(pair)
-    }
-
-    rotated.forEach(([x, y]) => {
+  // Phase 3: Eliminate rotation.
+  while (Object.values(preferences).some(prefs => prefs.length > 1)) {
+    const [person] = Object.entries(preferences).find(([person, prefs]) => prefs.length > 1)
+    if (!person) break
+    const rotations = getRotation(preferences, person)
+    for (const [x, y] of rotations) {
       preferences[x].shift()
       preferences[y].pop()
-    })
-
-    rotated.forEach(([x, y]) => {
-      let last = preferences[y][preferences[y].length - 1]
-      while (preferences[last]?. [0] !== y) {
+    }
+    for (const [x, y] of rotations) {
+      let lastY = tail(preferences[y])
+      if (!lastY) {
+        throw new Error('no stable matching: list is empty')
+      }
+      if (head(preferences[lastY]) !== y) {
+        preferences[lastY] = preferences[lastY].filter(person => person !== y)
         preferences[y].pop()
-        last = preferences[y][preferences[y].length - 1]
-        if (!preferences[y].length) throw new Error('no stable matching')
-      }
-    })
-  }
-
-  const output = {}
-  for (let key in preferences) {
-    if (preferences[key].length === 1) {
-      const head = preferences[key][0]
-      if (preferences[head][0] === key) {
-        output[key] = head
-        delete preferences[key]
-        delete preferences[head]
-      }
-    } else {
-      throw new Error('no stable matching')
-    }
-  }
-  return output
-}
-
-// For the first time, find the second item, use it as a key to find the next last item, and continue until a cycle is found.
-function getRotation(preferences, person = Object.keys(preferences)[0], secondOrLast = true, items = [person]) {
-  if (items.length > Object.keys(preferences).length) {
-    // Find rotation at any even index.
-    for (let i = 0; i < items.length; i += 2) {
-      const p = items[i]
-      if (items.slice(i + 2).includes(p)) {
-        const lastIdx = items.slice(i + 2).findIndex(it => it === p)
-        const rotation = items.slice(i, i + 2 + lastIdx)
-        rotation.push(rotation.shift())
-        return rotation
       }
     }
-    throw new Error(`no rotation found (key=${person})`)
   }
-  const prefs = preferences[person]
-  if (prefs.length < 2) {
-    return items
-  }
-  const nextPerson = prefs[secondOrLast ? 1 : prefs.length - 1]
-
-  // NOTE: This only find rotation at the first index. It should actually find the rotation at any index.
-  if (items?. [0] === nextPerson) {
-    items.shift()
-    items.push(nextPerson)
-    return items
-  }
-
-  items.push(nextPerson)
-  return getRotation(preferences, nextPerson, !secondOrLast, items)
-}
-
-function clone(object) {
-  const result = {}
-  for (let key in object) {
-    result[key] = [...object[key]]
+  const result = []
+  for (let person in preferences) {
+    if (preferences[person].length === 0) {
+      throw new Error('no stable matching: empty list')
+    }
+    const match = preferences[person][0]
+    if (preferences[match][0] === person) {
+      result.push([person, preferences[person][0]])
+      delete preferences[person]
+      delete preferences[match]
+    }
   }
   return result
 }
 
-function stringify(o) {
-  return Object.entries(o).reduce((acc, [key, preferences]) => {
-    acc[key] = preferences.map(item => item.toString())
-    return acc
-  }, {})
-}
-
-function uniqueArray(arr) {
-  return arr.length === new Set(arr).size
-}
-
-function duplicateInArray(arr) {
-  if (uniqueArray(arr)) return []
-
-  const counter = {}
-  for (let item of arr) {
-    if (!counter[item]) counter[item] = 0
-    counter[item]++
-  }
-  return Object
-    .entries(counter)
-    .filter(([key, count]) => count > 1)
-    .map(([key, count]) => key)
-}
-
-function validate(o) {
-  const people = Object.keys(o).length
-  for (let key in o) {
-    if (o[key].includes(key)) {
-      throw new Error('cannot match against ownself')
+function getRotation(preferences, person, secondOrLast = true, p = [person], q = []) {
+  const prefs = preferences[person]
+  if (!prefs) throw new Error('getRotationError: no stable matching: list is empty')
+  const target = prefs[secondOrLast ? 1 : prefs.length - 1]
+  const rotationExists = !secondOrLast && p.includes(target)
+  secondOrLast ? q.push(target) : p.push(target)
+  if (rotationExists) {
+    // Find the tail of the cycle.
+    while (head(p) !== target) {
+      p.shift()
+      q.shift()
     }
-    const duplicates = duplicateInArray(o[key])
-    if (duplicates.length) {
-      throw new Error(`${key} has duplicate keys ${duplicates}`)
+    p.shift()
+    const rotation = []
+    while (p.length) {
+      const k = p.shift()
+      const v = q.shift()
+      rotation.push([k, v])
     }
+    rotation.unshift(rotation.pop())
+    return rotation
   }
+  return getRotation(preferences, target, !secondOrLast, p, q)
+}
+
+function tail(arr) {
+  return arr[arr.length - 1]
+}
+
+function head(arr) {
+  return arr[0]
 }
 
 function shuffleArray(array) {
@@ -237,21 +198,20 @@ function shuffleArray(array) {
   }
 }
 
-function generatePreferences(n = 30, start = 1) {
+function generateList(n = 10) {
   const result = {}
+  const arr = Array(n).fill(0).map((_, i) => i + 1)
   for (let i = 0; i < n; i++) {
-    result[i + start] = Array(n)
-      .fill(start)
-      .flatMap((n, idx) => idx + n === i + start ? [] : [(idx + n).toString()])
-    shuffleArray(result[i + start])
+    const tmp = new Set(arr)
+    tmp.delete(i + 1)
+    result[i + 1] = [...tmp]
+    shuffleArray(result[i + 1])
   }
   return result
 }
 
-
-console.log(stableRoommateProblem(stringify(preferences)))
-console.log(stableRoommateProblem(stringify(preferences1)))
-console.log(stableRoommateProblem(stringify(preferences2)))
-console.log(stableRoommateProblem(stringify(preferences3)))
-// This will sometimes generate a result - refresh for different possiblity.
-console.log(stableRoommateProblem(stringify(generatePreferences(30))))
+console.log('1', stableRoommateProblem(preferences1))
+console.log('2', stableRoommateProblem(stringify(preferences2)))
+console.log('3', stableRoommateProblem(stringify(preferences3)))
+console.log('4', stableRoommateProblem(stringify(preferences4)))
+console.log('5', stableRoommateProblem(stringify(generateList(40))))
